c9ba3932aaf609a366f9b0939d660752bc416e60
Jasper <49565505+jasperdoan@users.noreply.github.com>
2023-07-07 02:35:13 -0700

    Update graphing

--- python_pkg/main.py
+++ python_pkg/main.py
@@ -1,10 +1,20 @@
 import bnlearn as bn
+import pandas as pd
 
-df = bn.import_example()
-# df = pd.read_csv('sprinkler_data.csv')
+# # df = bn.import_example()
+# df = pd.read_csv('data\osd_489\LSDS-15_microCT_alwoodTRANSFORMED.csv')
+# df = df.iloc[:, 3:]
 
+# model = bn.structure_learning.fit(df)
+# model = bn.independence_test(model, df)     # Compute edge strength with the chi_square test statistic
+# G = bn.plot(model)
 
-model = bn.structure_learning.fit(df)
-# Compute edge strength with the chi_square test statistic
-model = bn.independence_test(model, df)
-G = bn.plot(model)
+# Read the csv file
+l9 = pd.read_csv('data\osd_351\LSDS-9_microCT_turnerTRANSFORMED.csv')
+l40 = pd.read_csv('data\osd_477\LSDS-40_microCT_LSDS-40_microCT_KoTRANSFORMED.csv', skiprows=1)
+l15 = pd.read_csv('data\osd_489\LSDS-15_microCT_alwoodTRANSFORMED.csv')
+
+# Print out the header columns 
+print(l9.columns, '\n\n')
+print(l40.columns, '\n\n')
+print(l15.columns, '\n\n')
